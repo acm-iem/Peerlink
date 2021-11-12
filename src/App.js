5b2d6d378c837c31ccb7009e59306ef65f601cc0
GlitchFlex <pratyugna@gmail.com>
2021-11-12 23:02:52 +0530

    basic login page added

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from "./components/Header"
+import Form from "./components/Form"
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <Form/>
+    </>
+  );
+}
+
+export default App;
