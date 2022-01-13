9cbd3cf6452c505920c624b9d64eecb43a4db007
Ritam Mondal <60252836+Titan-Ritam@users.noreply.github.com>
2022-01-14 00:04:11 +0530

    Update conn.js
    changes some credentials info

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const db = 'mongodb+srv://titan:71737977@cluster0.3j0el.mongodb.net/peerlink?retryWrites=true&w=majority'
+const db = 'mongodb+srv://{username}:{password}@cluster0.3j0el.mongodb.net/peerlink?retryWrites=true&w=majority'
 mongoose.connect(db).then(()=>{
     console.log("connection successfull")
 }).catch((err)=>{
