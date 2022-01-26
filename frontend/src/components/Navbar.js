da0992e04ee18981521ea54da0fdeb1856d3bcab
Debayan <sdebayan999@gmail.com>
2022-01-26 19:47:33 +0530

    routing added

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,19 +1,41 @@
 import React from 'react'
+import {Link, useNavigate} from 'react-router-dom'
 
 export default function Navbar(props) {
+    const navigate = useNavigate();
+    const setNav = () => {
+        switch (props.val) {
+            case "Login/Signup":
+                navigate('/login')
+                break;
+
+            case "Log In":
+                navigate('/login')
+                break;
+
+            case "Sign Up":
+                navigate('/signup')
+                break;
+        
+            default:
+                break;
+        }
+      }
+    
     return (
         <>
         <nav id="nav">
                     <div id='logo'>PeerLink</div>
                     <ul type="none">
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">About Us</a></li>
-                        <li><a href="#">Contact</a></li>
-                        <li><a href="#">Download</a></li>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/chat">Chat</Link></li>
+                        <li><Link to="/about">About Us</Link></li>
+                        <li><Link to="/contact">Contact</Link></li>
+                        <li><Link to="/download">Download</Link></li>
                        
                     </ul>
                     <div className="navLeft">
-                        <button className='butt'>{props.val}</button>
+                        <button className='butt' onClick={setNav}>{props.val}</button>
                         <img src="" alt="" />
                     </div>
                 </nav>
