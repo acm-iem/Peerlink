5844cb31df677653e41f26eccdd7e2bb27336484
Debayan <sdebayan999@gmail.com>
2022-02-08 10:00:39 +0530

    connect with db

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+require('dotenv').config()
 
-const db = 'mongodb+srv://peerlinkdb:peerlinkdb@peerlinkdb.0xdn0.mongodb.net/peerlinkdb?retryWrites=true&w=majority'
+const db = `mongodb+srv://${ process.env.DB_USER_NAME }:${ process.env.DB_PASSWORD }@peerlinkdb.0xdn0.mongodb.net/${ process.env.DB_COLLECTION }?retryWrites=true&w=majority`
 mongoose.connect(db).then(()=>{
     console.log("connection successfull")
 }).catch((err)=>{
